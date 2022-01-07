f05eeb84155e987cbad9daac733336b7223a8ebe
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2022-01-06 21:35:43 -0400

    add SwiftLint

---
+++ Pods/Target Support Files/Pods-Reed&Meet-Reed&MeetUITests/Pods-Reed&Meet-Reed&MeetUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Reed_Meet_Reed_MeetUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Reed_Meet_Reed_MeetUITests
+@end
---
+++ Pods/Target Support Files/Pods-Reed&Meet/Pods-Reed&Meet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Reed_Meet : NSObject
+@end
+@implementation PodsDummy_Pods_Reed_Meet
+@end
