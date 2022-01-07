129f9594fa4fe676d6b8d9f892043e580213b73e
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2022-01-07 16:31:34 -0400

    add AutoLayout

---
+++ Reed&Meet/Common/AutoLayout/NSLayoutConstraintType.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+enum NSLayoutConstraintType {
+    case equal
+    case greaterThanOrEqual
+    case lessThanOrEqual
+
+    func get() -> NSLayoutConstraint.Relation {
+        switch self {
+        case .equal:
+            return NSLayoutConstraint.Relation.equal
+        case .greaterThanOrEqual:
+            return NSLayoutConstraint.Relation.greaterThanOrEqual
+        case .lessThanOrEqual:
+            return NSLayoutConstraint.Relation.lessThanOrEqual
+        }
+    }
+}
+
